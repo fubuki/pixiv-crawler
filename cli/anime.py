60daabe061f7ca66b7804aae4b84ec4b8f7db207
fubuki <fubuki@users.noreply.github.com>
2018-03-08 22:10:55 +0800

    make cli command.

--- cli/anime.py
+++ cli/anime.py
@@ -3,23 +3,16 @@
 import cv2
 
 
-def face(filename, cascade_file="lbpcascade_animeface.xml"):
+def face(img, cascade_file="lbpcascade_animeface.xml"):
     cascade = cv2.CascadeClassifier(cascade_file)
-
-    img = cv2.imread(filename)
-
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
     gray = cv2.equalizeHist(gray)
 
     faces = cascade.detectMultiScale(gray,
                                      scaleFactor=1.1,
                                      minNeighbors=1,
                                      minSize=(24, 24))
-    return face
-
-
-    return None
+    return faces
 
 
 def img_sift(filename):
@@ -31,7 +24,7 @@ def img_sift(filename):
     return kp, des
 
 
-def compare(img1, img2):
+def search_object(object, image):
 
     FLANN_INDEX_KDTREE = 0
     index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
@@ -51,5 +44,5 @@ def compare(img1, img2):
                        matchesMask=matches_mask,
                        flags=0)
 
-    img3 = cv2.drawMatchesKnn(img1.img, img1.kp, img2.img, img2.kp, matches, None, **draw_params)
+    img3 = cv2.drawMatchesKnn(object.img, object.kp, image.img, image.kp, matches, None, **draw_params)
     return img3
--- cli/calculator.py
+++ cli/calculator.py
@@ -4,16 +4,18 @@ import cv2
 import fire
 import anime
 
+
 class Search(object):
     def double(self, number):
         return 2 * number
 
-    def face(self, source, dest):
+    def image_face(self, source, dest):
         cascade_file = "lbpcascade_animeface.xml"
         if not os.path.isfile(cascade_file):
             raise RuntimeError("%s: not found" % cascade_file)
 
-        faces = anime.face(source, cascade_file)
+        source_img = cv2.imread(source)
+        faces = anime.face(source_img, cascade_file)
 
         if len(faces) > 0:
             img = cv2.imread(source)
@@ -26,6 +28,66 @@ class Search(object):
                 if dst is not None:
                     cv2.imwrite(dest, dst)
 
+    def anime_face(self, filename):
+
+        cap = cv2.VideoCapture(filename)
+
+        framenum = 0
+        faceframenum = 0
+        color = (0, 0, 255)
+
+        while (cap.isOpened()):
+            framenum += 1
+
+            ret, image = cap.read()
+            if not ret:
+                break
+
+            if framenum % 50 == 0:
+                faces = anime.face(image)
+                if len(faces) == 0:
+                    continue
+
+                for rect in faces:
+                    x = rect[0]
+                    y = rect[1]
+                    width = rect[2]
+                    height = rect[3]
+                    dst = image[y:y + height, x:x + width]
+                    resized_dst = cv2.resize(dst, (64, 64))
+                    faceframenum += 1
+
+        cap.release()
+
+
+    def search_object(self, object, image):
+        object_kp, object_des = anime.img_sift(object)
+        image_kp, image_des = anime.img_sift(image)
+
+        object_img = cv2.imread(object, 0)
+        image_img = cv2.imread(image, 0)
+
+        FLANN_INDEX_KDTREE = 0
+        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+        search_params = dict(checks=50)
+
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+        matches = flann.knnMatch(object_des, image_des, k=2)
+        matches_mask = [[0, 0] for i in xrange(len(matches))]
+
+        for i, (m, n) in enumerate(matches):
+            if m.distance < 0.7 * n.distance:
+                matches_mask[i] = [1, 0]
+
+        draw_params = dict(matchColor=(0, 255, 0),
+                           singlePointColor=(255, 0, 0),
+                           matchesMask=matches_mask,
+                           flags=0)
+
+        img3 = cv2.drawMatchesKnn(object_img, object_kp, image_img, image_kp, matches, None, **draw_params)
+
+        cv2.imwrite("search_object.png", img3)
 
 
 if __name__ == '__main__':
